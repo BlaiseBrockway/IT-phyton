ecec1c98f4c332f863f12b86e21cd114e09e7b8f
Blaise Brockway <blaise9@rocketmail.com>
2019-09-18 13:35:21 -0400

    add R6.py

---
+++ R6.py
@@ -0,0 +1,8 @@
+from banner import banner
+banner("Siege Situation", "Blaise")
+
+print("Welcome to Siege Situations")
+print("You will be tested on your skill in Rainbow Six Siege")
+print("Good Luck!")
+
+op = input("Pick your Operator: Frost, Castle, Kapcan, Caviera,
